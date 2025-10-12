d9558afa60eb045b847d0c64c644eec052c79f58
LongND <longnd@sphinxjsc.com>
2025-10-12 15:54:41 +0700

    mimi

--- src/components/wedding/Header.tsx
+++ src/components/wedding/Header.tsx
@@ -106,11 +106,12 @@ export default function Header() {
                     variant="primary"
                     onClick={openLoginModal}
                   >
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <svg className="max-h-[24px] max-w-[24px]" width="full" height="full" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path d="M13.3335 2.66699H2.66683C1.93045 2.66699 1.3335 3.26395 1.3335 4.00033V12.0003C1.3335 12.7367 1.93045 13.3337 2.66683 13.3337H13.3335C14.0699 13.3337 14.6668 12.7367 14.6668 12.0003V4.00033C14.6668 3.26395 14.0699 2.66699 13.3335 2.66699Z" stroke="white" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round" />
                       <path d="M14.6668 4.66699L8.68683 8.46699C8.48101 8.59594 8.24304 8.66433 8.00016 8.66433C7.75729 8.66433 7.51932 8.59594 7.3135 8.46699L1.3335 4.66699" stroke="white" strokeWidth="1.33333" strokeLinecap="round" strokeLinejoin="round" />
                     </svg>
-                    <span className="ml-2 font-[600]">Đăng nhập bằng Email</span>
+                    <span className="lg:hidden block ml-2 font-[600]">Đăng nhập</span>
+                    <span className="hidden lg:block ml-2 font-[600]">Đăng nhập bằng Email</span>
                   </Button>
                 </>
               )}
